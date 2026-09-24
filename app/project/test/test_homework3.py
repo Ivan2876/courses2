5155339624be6fd1cd7b1de5862112d4bb2b2c99
“John <john@email.com>
2026-09-24 17:06:24 +0300

    homework3

--- app/project/test/test_homework3.py
+++ app/project/test/test_homework3.py
@@ -8,9 +8,9 @@ class TestShoppingCart:
         shopping_cart.add_item(name="Шапка", price=100, quantity=2)
         shopping_cart.add_item(name="Шкарпетки", price=50, quantity=4)
         shopping_cart.add_item(name="Книга біологія 10 клас", price=150, quantity=1)
+        assert shopping_cart.get_summa() == 850
 
     def test_remove(self, shopping_cart: ShoppingCart):
         shopping_cart.remove_item(name="Куртка")
-
-    def test_get_summa(self, shopping_cart: ShoppingCart):
         assert shopping_cart.get_summa() == 550
+
